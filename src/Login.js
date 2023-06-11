d1e7b038ef547d711e8d2d36f62d0f16edd2a4b7
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2023-06-11 19:58:12 +0530

    Add signup & login

--- src/Login.js
+++ src/Login.js
@@ -1,86 +1,106 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from './components/Header.js';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "./components/Header.js";
 import {
-  Box,
-  Container,
-  FormControl,
-  FormLabel,
-  Input,
-  Text,
-  Button,
-  HStack,
-  Flex,
-} from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+    Box,
+    Container,
+    FormControl,
+    FormLabel,
+    Input,
+    Text,
+    Button,
+    HStack,
+    Flex,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import axios from "axios";
 export default function Login() {
-  let navigate = useNavigate();
-  let [email, setEmail] = useState('');
-  let [password, setPassword] = useState('');
-  function handleSignup() {
-    navigate('/signup');
-  }
+    let navigate = useNavigate();
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [error, setError] = useState("");
+    function handleSignup() {
+        navigate("/signup");
+    }
 
-  function handleLogin() {
-    if (email == '' || password == '') return;
-    console.log(email, password);
-  }
-  return (
-    <Box>
-      <Header />
-      <Flex justifyContent="center" alignItems="center" height="80vh">
-        <Container
-          maxW="md"
-          padding="4"
-          borderColor="gray.200"
-          borderRadius="8"
-          boxShadow="xl"
-          marginTop="4"
-          bg="purple.50"
-          centerContent
-        >
-          <Text fontSize="2xl">Login</Text>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input
-              focusBorderColor="purple.600"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input
-              focusBorderColor="purple.600"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <Box w="100%">
-            <HStack justify="space-around" marginTop="4">
-              <Button
-                size="md"
-                variant="outline"
-                colorScheme="purple"
-                onClick={handleSignup}
-              >
-                Signup
-              </Button>
-              <Button
-                size="md"
-                variant="solid"
-                colorScheme="purple"
-                rightIcon={<ArrowForwardIcon />}
-                onClick={handleLogin}
-              >
-                Login
-              </Button>
-            </HStack>
-          </Box>
-        </Container>
-      </Flex>
-    </Box>
-  );
+    async function handleLogin() {
+        try {
+            if (email == "" || password == "") {
+                throw new Error("Email & Password required!");
+            }
+            let res = await axios.post("http://localhost:8080/login", {
+                email,
+                password,
+            });
+            if (res.data.success) {
+                localStorage.setItem("jwt-token", res.data.data.token);
+                navigate("/app");
+            } else {
+                throw new Error(res.data.error.message);
+            }
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+    return (
+        <Box>
+            <Header />
+            <Flex justifyContent="center" alignItems="center" height="80vh">
+                <Container
+                    maxW="md"
+                    padding="4"
+                    borderColor="gray.200"
+                    borderRadius="8"
+                    boxShadow="xl"
+                    marginTop="4"
+                    bg="purple.50"
+                    centerContent
+                >
+                    <Text fontSize="md" color="red">
+                        {error}
+                    </Text>
+                    <Text fontSize="2xl">Login</Text>
+                    <FormControl>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <Box w="100%">
+                        <HStack justify="space-around" marginTop="4">
+                            <Button
+                                size="md"
+                                variant="outline"
+                                colorScheme="purple"
+                                onClick={handleSignup}
+                            >
+                                Signup
+                            </Button>
+                            <Button
+                                size="md"
+                                variant="solid"
+                                colorScheme="purple"
+                                rightIcon={<ArrowForwardIcon />}
+                                onClick={handleLogin}
+                            >
+                                Login
+                            </Button>
+                        </HStack>
+                    </Box>
+                </Container>
+            </Flex>
+        </Box>
+    );
 }
--- src/Signup.js
+++ src/Signup.js
@@ -1,125 +1,139 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from './components/Header.js';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "./components/Header.js";
 import {
-  Box,
-  Container,
-  FormControl,
-  FormLabel,
-  Input,
-  Text,
-  Button,
-  HStack,
-  Flex,
-} from '@chakra-ui/react';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
-import axios from 'axios';
+    Box,
+    Container,
+    FormControl,
+    FormLabel,
+    Input,
+    Text,
+    Button,
+    HStack,
+    Flex,
+} from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import axios from "axios";
 
 export default function Signup() {
-  let navigate = useNavigate();
-  let [email, setEmail] = useState('');
-  let [password, setPassword] = useState('');
-  let [phone, setPhone] = useState('');
-  let [error, setError] = useState('');
+    let navigate = useNavigate();
+    let [name, setName] = useState("");
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
+    let [phone, setPhone] = useState("");
+    let [error, setError] = useState("");
 
-  async function handleSignup() {
-    if (
-      !email ||
-      email == '' ||
-      !password ||
-      password == '' ||
-      !phone ||
-      phone == ''
-    ) {
-      setError('All fields are required!');
-      return;
+    async function handleSignup() {
+        if (
+            !name ||
+            name == "" ||
+            !email ||
+            email == "" ||
+            !password ||
+            password == "" ||
+            !phone ||
+            phone == ""
+        ) {
+            setError("All fields are required!");
+            return;
+        }
+        try {
+            let res = await axios.post("http://localhost:8080/signup", {
+                name,
+                email,
+                password,
+                phone,
+            });
+            if (res.data.success) {
+                navigate("/login");
+            } else {
+                throw new Error(res.data.error.message);
+            }
+        } catch (err) {
+            setError(err.message);
+        }
     }
-    try {
-      let res = await axios.post(
-        (url = '/signup'),
-        (data = { email, password, phone })
-      );
-      if (res.status == 200) {
-        navigate('/app');
-      } else {
-        throw new Error('Internal Server Error');
-      }
-    } catch (err) {
-      setEmail(err.msg);
-    }
-  }
 
-  function handleLogin() {
-    navigate('/login');
-  }
-  return (
-    <Box>
-      <Header />
-      <Flex justifyContent="center" alignItems="center" height="80vh">
-        <Container
-          maxW="md"
-          padding="4"
-          borderColor="gray.200"
-          borderRadius="8"
-          boxShadow="xl"
-          marginTop="4"
-          bg="purple.50"
-          centerContent
-        >
-          <Text fontSize="md" color="red">
-            {error}
-          </Text>
-          <Text fontSize="2xl">Signup</Text>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input
-              focusBorderColor="purple.600"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input
-              focusBorderColor="purple.600"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Phone</FormLabel>
-            <Input
-              focusBorderColor="purple.600"
-              type="tel"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </FormControl>
-          <Box w="100%">
-            <HStack justify="space-around" marginTop="4">
-              <Button
-                size="md"
-                variant="outline"
-                colorScheme="purple"
-                onClick={handleLogin}
-              >
-                Login
-              </Button>
-              <Button
-                size="md"
-                variant="solid"
-                colorScheme="purple"
-                rightIcon={<ArrowForwardIcon />}
-                onClick={handleSignup}
-              >
-                Signup
-              </Button>
-            </HStack>
-          </Box>
-        </Container>
-      </Flex>
-    </Box>
-  );
+    function handleLogin() {
+        navigate("/login");
+    }
+    return (
+        <Box>
+            <Header />
+            <Flex justifyContent="center" alignItems="center" height="80vh">
+                <Container
+                    maxW="md"
+                    padding="4"
+                    borderColor="gray.200"
+                    borderRadius="8"
+                    boxShadow="xl"
+                    marginTop="4"
+                    bg="purple.50"
+                    centerContent
+                >
+                    <Text fontSize="md" color="red">
+                        {error}
+                    </Text>
+                    <Text fontSize="2xl">Signup</Text>
+                    <FormControl>
+                        <FormLabel>Name</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Email</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel>Phone</FormLabel>
+                        <Input
+                            focusBorderColor="purple.600"
+                            type="tel"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                    </FormControl>
+                    <Box w="100%">
+                        <HStack justify="space-around" marginTop="4">
+                            <Button
+                                size="md"
+                                variant="outline"
+                                colorScheme="purple"
+                                onClick={handleLogin}
+                            >
+                                Login
+                            </Button>
+                            <Button
+                                size="md"
+                                variant="solid"
+                                colorScheme="purple"
+                                rightIcon={<ArrowForwardIcon />}
+                                onClick={handleSignup}
+                            >
+                                Signup
+                            </Button>
+                        </HStack>
+                    </Box>
+                </Container>
+            </Flex>
+        </Box>
+    );
 }
